34f81460768a38ed676fb89f52b52e92501fa1f3
Lucas Santos <lucasarosss@gmail.com>
2024-03-01 17:34:11 -0300

    Atualização do layout

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Cursoemvideo_VerificadorDeIdade
 Verificador de idade
+
+https://accounts.google.com/lifecycle/steps/signup/birthdaygender?TL=AHNYTISpgPcEJiXI4J7rynrxgpjjcHZhNE3lr9kuCdavQx0hk9fiDfwInvppr31K&continue=https%3A%2F%2Fwww.google.com%2F&dsh=S469983647%3A1706566637558926&ec=GAZAmgQ&flowEntry=SignUp&flowName=GlifWebSignIn&hl=pt-PT&ifkv=ASKXGp3rZFO8Lub9Wq1TRz2kutsb6x93b77tGtIQrGeILSHMYZHt-wUNNJbdJURc_A3K_4ojuTyTqQ&theme=glif
--- script.js
+++ script.js
@@ -1,53 +1,68 @@
+let botaoVerificar = document.querySelector('.form__button');
+botaoVerificar.addEventListener('click', ()=> {
+    verificar();
+})
+
 function verificar() {
     let data = new Date(); //data e a hora atuais
     let anoAtual = data.getFullYear(); //ano atual
-    let anoNascimento = document.getElementsByName('ano')[0].value;
-    let sexo = document.getElementsByName('sexo');
+    let anoNascimento = document.getElementsByName('year')[0].value;
+    let sexo = document.getElementsByName('gender');
     let idade =  anoAtual - anoNascimento;
-    
-    if (anoNascimento == 0 || anoNascimento > anoAtual) {
-        alert('Verifique os dados e tente novamente')
+
+    if (anoNascimento < 1900 || anoNascimento > anoAtual) {
+        alert('Ano de nascimento inválido!')
 
     } else {
+        let textoHomem = `Detectamos Homem de ${idade} anos`;
+        let textoBebe = idade == 1 ? `Detectamos Bebê de ${idade} ano` : `Detectamos Bebê de ${idade} anos`;
+        let textoCrianca = `Detectamos Criança de ${idade} anos`;
+        let textoAdolescente = `Detectamos Adolescente de ${idade} anos`;
+        let textoJovem = `Detectamos Jovem de ${idade} anos`;
+        let textoSenhor = `Detectamos Senhor de ${idade} anos`;
+
         if (sexo[0].checked) {
-            let textoM = `Detectamos Homem de ${idade} anos`;
-            
-            if (idade <= 7) {
-                resultado(textoM, 'imagens/crianca-M.png');
+            if (idade <= 3) {
+                resultado(textoBebe, 'imagens/bebe-m.png');
+            } else if (idade <= 12) {
+                resultado(textoCrianca, 'imagens/crianca-m.png');
             } else if (idade <= 17) {
-                resultado(textoM, 'imagens/adolescente-M.png');
-            } else if (idade < 30) {
-                resultado(textoM, 'imagens/jovem-M.jpg');
-            } else if (idade <= 49) {
-                resultado(textoM, 'imagens/adulto-M.png');
+                resultado(textoAdolescente, 'imagens/adolescente-m.png');
+            } else if (idade <= 29) {
+                resultado(textoJovem, 'imagens/jovem-m.jpg');
+            } else if (idade <= 60) {
+                resultado(textoHomem, 'imagens/homem-m.png');    
             } else {
-                resultado(textoM, 'imagens/senhor-M.png');
+                resultado(textoSenhor, 'imagens/senhor-m.png');
             }
 
         } else {
-            let textoF = `Detectamos mulher de ${idade} anos`;
+            let textoMulher = `Detectamos Mulher de ${idade} anos`;
+            let textoSenhora = `Detectamos Senhora de ${idade} anos`;
 
-            if (idade <= 7) {
-                resultado(textoF, 'imagens/crianca-F.png')
-            } else if (idade <= 17){
-                resultado(textoF, 'imagens/adolescente-F.png')
-            } else if (idade < 30){
-                resultado(textoF, 'imagens/jovem-F.png')
-            } else if (idade <= 49){
-                resultado(textoF, 'imagens/adulta-F.png')
-            }else {
-                resultado(textoF, 'imagens/senhora-F.png')
+            if (idade <= 3) {
+                resultado(textoBebe, 'imagens/bebe-f.png')
+            } else if (idade <= 12) {
+                resultado(textoCrianca, 'imagens/crianca-f.jpg')
+            } else if (idade <= 17) {
+                resultado(textoAdolescente, 'imagens/adolescente-f.png')
+            } else if (idade <= 29) {
+                resultado(textoJovem, 'imagens/jovem-f.png')
+            } else if (idade <= 60) {
+                resultado(textoMulher, 'imagens/mulher-f.png')
+            } else {
+                resultado(textoSenhora, 'imagens/senhora-f.png')
             }
         }
     }
 }
 
-
 function resultado(texto, imagem) {
-    let textoResultado = document.querySelector('.resultado');
+    let textoResultado = document.querySelector('.result__text');
     textoResultado.innerText = texto;
 
-    let imagemResultado = document.querySelector('.imagem');
+    let imagemResultado = document.querySelector('.result__image');
     imagemResultado.src = imagem;
+    imagemResultado.style.display = 'block'
 }
 
